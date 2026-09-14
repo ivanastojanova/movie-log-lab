3cfb6590a559156e1577b8ddfb5966d74bfb9216
Ivana <ivana.kareva@gmail.com>
2026-09-14 12:55:41 +0200

    open/close modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ALL_MOVIES } from "./data/movies";
 import MovieItem from "./components/movie-item";
 import MovieForm from "./components/movie-form";
@@ -7,18 +8,39 @@ const currentMovie = null;
 
 export default function App() {
   const movies = ALL_MOVIES.items;
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   function handleSave(movieData) {
     console.log(movieData);
+    setIsFormOpen(false);
   }
 
   function handleCancel() {
     console.log("cancel");
+    setIsFormOpen(false);
   }
 
   return (
     <div className="app">
-      <MovieForm movie={currentMovie} onSave={handleSave} onCancel={handleCancel} />
+      <button className="btn btn-primary" onClick={() => setIsFormOpen(true)}>
+        Add Movie
+      </button>
+
+      {isFormOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+          onClick={handleCancel}
+        >
+          <div onClick={(e) => e.stopPropagation()}>
+            <MovieForm
+              movie={currentMovie}
+              onSave={handleSave}
+              onCancel={handleCancel}
+            />
+          </div>
+        </div>
+      )}
+
       <div className="movie-list">
         {movies.map((movie) => (
           <MovieItem key={movie.id} movie={movie} />
